69dad39d022e9c170871e3957e11ec734f0fc4ec
vunewbie <vulocninh1@gmail.com>
2025-02-04 20:14:25 +0700

    Custom User and Profile Model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,6 +133,8 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+AUTH_USER_MODEL = 'userauths.User'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(unique=True, max_length=100)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=100)
+    otp = models.CharField(unique=True, max_length=100)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        email_username = self.email.split('@')[0]
+        if self.full_name == '' or self.full_name is None:
+            self.full_name = email_username
+        if self.username == '' or self.username is None:
+            self.username = email_username
+        super(User, self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='user_folder', default='default-user.jpg', null=True, blank=True)
+    full_name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    about = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.full_name:
+            return self.full_name
+        else:
+            return self.user.full_name
+    
+    def save(self, *args, **kwargs):
+        if self.full_name == '' or self.full_name is None:
+            self.full_name = self.user.full_name
+        super(Profile, self).save(*args, **kwargs)
